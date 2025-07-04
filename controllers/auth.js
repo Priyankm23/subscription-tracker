1c322699a3c483385fd49f9d9b5784698357a5f2
Priyank Moradiya <153429763+Priyankm23@users.noreply.github.com>
2025-07-04 09:35:33 +0530

    fix: update Google OAuth callback URL to production

--- controllers/auth.js
+++ controllers/auth.js
@@ -79,7 +79,7 @@ export const signIn= async (req,res,next)=>{
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:5000/auth/google/callback"
+    callbackURL: "https://subscription-tracker.onrender.com/auth/google/callback"
   },async (accessToken, refreshToken, profile, done) => {
     const session=await mongoose.startSession();
     session.startTransaction();
